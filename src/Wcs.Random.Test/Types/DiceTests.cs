22c974f3cbf983cd87243722b2a312dbb232f424
Ben Walton <walton713@gmail.com>
2022-05-28 20:15:56 -0500

    refactoring tests

--- src/Wcs.Random.Test/Types/DiceTests.cs
+++ src/Wcs.Random.Test/Types/DiceTests.cs
@@ -1,194 +1,129 @@
 using System.Collections.Generic;
 using FluentAssertions;
-using MathNet.Numerics.Random;
 using Wcs.Random.Types;
 using Xunit;
 
 namespace Wcs.Random.Test.Types;
 
-public class DiceTests
+public class DiceTests : RandomizerTests
 {
     private readonly Dice _sut;
 
     public DiceTests()
     {
-        _sut = new Dice(new MersenneTwister(true));
+        _sut = Randomizer.Dice;
     }
 
     [Theory]
     [Repeat(10)]
     public void D2ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 2; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(2);
 
         for (var i = 0; i < 2000; i++)
         {
             results[_sut.D2()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D4ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 4; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(4);
 
         for (var i = 0; i < 4000; i++)
         {
             results[_sut.D4()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D6ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 6; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(6);
 
         for (var i = 0; i < 6000; i++)
         {
             results[_sut.D6()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D8ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 8; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(8);
 
         for (var i = 0; i < 8000; i++)
         {
             results[_sut.D8()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D10ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 10; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(10);
 
         for (var i = 0; i < 10000; i++)
         {
             results[_sut.D10()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D12ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 12; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(12);
 
         for (var i = 0; i < 12000; i++)
         {
             results[_sut.D12()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D20ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 20; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(20);
 
         for (var i = 0; i < 20000; i++)
         {
             results[_sut.D20()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
     [Repeat(10)]
     public void D100ReturnsCorrectValues()
     {
-        var results = new Dictionary<int, int>();
-
-        for (var i = 1; i <= 100; i++)
-        {
-            results.Add(i, 0);
-        }
+        var results = InitialResults(100);
 
         for (var i = 0; i < 100000; i++)
         {
             results[_sut.D100()]++;
         }
 
-        foreach (var result in results)
-        {
-            result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
-        }
+        CheckResults(results);
     }
 
     [Theory]
@@ -198,6 +133,18 @@ public class DiceTests
     [InlineData(50)]
     public void DSidesReturnsCorrectValues(int sides)
     {
+        var results = InitialResults(sides);
+
+        for (var i = 0; i <= sides * 1000; i++)
+        {
+            results[_sut.DSides(sides)]++;
+        }
+
+        CheckResults(results);
+    }
+
+    private static Dictionary<int, int> InitialResults(int sides)
+    {
         var results = new Dictionary<int, int>();
 
         for (var i = 1; i <= sides; i++)
@@ -205,11 +152,11 @@ public class DiceTests
             results.Add(i, 0);
         }
 
-        for (var i = 0; i <= sides * 1000; i++)
-        {
-            results[_sut.DSides(sides)]++;
-        }
+        return results;
+    }
 
+    private static void CheckResults(Dictionary<int, int> results)
+    {
         foreach (var result in results)
         {
             result.Value.Should().BeInRange(850, 1150, $"{result.Key} was out of range");
