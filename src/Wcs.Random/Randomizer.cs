6c2afaa0061353e3c6831cb55b137cba750f2ea0
Ben Walton <walton713@gmail.com>
2022-05-28 19:12:35 -0500

    simplify constructor

--- src/Wcs.Random/Randomizer.cs
+++ src/Wcs.Random/Randomizer.cs
@@ -5,13 +5,10 @@ namespace Wcs.Random
 {
     public class Randomizer
     {
-        private System.Random _rng;
-
         public Randomizer()
         {
-            _rng = new MersenneTwister(true);
-
-            Dice = new Dice(_rng);
+            System.Random rng = new MersenneTwister(true);
+            Dice = new Dice(rng);
         }
 
         public Dice Dice { get; }
